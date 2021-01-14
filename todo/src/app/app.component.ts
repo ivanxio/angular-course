1a4a92bcdfb0017cfba05ab8a2cf7dce10a12e02
Iván García Martínez <ivan.garcia@amk-technologies.com>
2021-01-14 17:56:05 -0600

    Displaying data to the User

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  /**
+   * The TodoList example to show.
+   */
+  private list = new TodoList(
+    "Bob", 
+    [
+      new TodoItem('Go for run', true),
+      new TodoItem('Get flowers'),
+      new TodoItem('Collect tickets')
+    ]
+  );
+
+  /**
+   * Getter to the username.
+   */
+  public get username() : string {
+    return this.list.user;
+  }
+
+  /**
+   * Getter to the complete items count.
+   */
+  public get itemCount() : number {
+    return this.list.items.filter(item => !item.complete).length;
+  }  
 }
