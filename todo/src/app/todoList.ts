a0441444005d0632cc1ce5db948c0b22de46476b
Iván García Martínez <ivan.garcia@amk-technologies.com>
2021-01-14 15:21:17 -0600

    The todoList data model was added.

---
+++ todo/src/app/todoList.ts
@@ -0,0 +1,29 @@
+import { from } from "rxjs";
+
+import { TodoItem } from "./todoItem";
+
+export class TodoList {
+
+    /**
+     * Creates a new instance.
+     *
+     * @param user The user.
+     * @param todoItems The items list. 
+     */
+    constructor(public user: string, private todoItems: TodoItem[] = []) {
+    }
+
+    /**
+     * Getter to the items list.
+     */
+    public get items() : readonly TodoItem[] {
+        return this.todoItems;
+    }
+
+    /**
+     * Method to add items to the list.
+     */
+    public addItem(task: string) {
+        this.todoItems.push(new TodoItem(task));
+    }
+}
